c5087ea1f8718f68e47223c4818db9419b6c6a3c
Solutions Mateus <solutionsmateus@gmail.com>
2025-11-26 14:52:52 +0000

    .

--- src/data/apis.ts
+++ src/data/apis.ts
@@ -14,23 +14,21 @@ export const apis: Api[] = [
     id: "concorrencia",
     name: "Concorrencia API",
     description: "Promoções e Campanhas de todos os supermercados.",
-    endpoint: "/api/v1/concorrencia",
+    endpoint: "https://solutionsmateus-encartes-download.hf.space/api/button",
     method: "GET, POST, PUT, DELETE",
     documentation: "Clique e pegue todos os dados da concorrencia",
     exampleRequest: `GET /api/v1/users/123
 Authorization: Bearer {token}`,
     exampleResponse: `{
-  "id": "Button",
-  "name": "John Doe",
-  "email": "john@example.com",
-  "created_at": "2024-01-01T00:00:00Z"
+  "status": "success",
+  "message": "Ação realizada com sucesso"
 }`,
   },
   {
     id: "Em Breve terão outros projetos",
     name: "Projetos.",
     description: "Clique e Veja.",
-    endpoint: "https://solutionsmateus-encartes-download.hf.space/api/button",
+    endpoint: "#",
     method: "GET, POST",
     exampleRequest: `POST /api/v1/novo-endpoint
     Authorization: Bearer {token}`,
