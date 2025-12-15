d6dd7df729eeb16544c347a6cb8d0c47fc8aee5d
Solutions Mateus <solutionsmateus@gmail.com>
2025-12-15 11:25:19 +0000

    .

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import ApiDetail from "./pages/ApiDetail";
 import NotFound from "./pages/NotFound";
+import Home from "./pages/Home";
 
 const queryClient = new QueryClient();
 
@@ -19,6 +20,7 @@ const App = () => (
           <Route path="/" element={<Index />} />
           <Route path="*" element={<NotFound />} />
           <Route path="/:supermercado/:acaoId" element={<ApiDetail />} />
+          <Route path="/" element={<Home />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
